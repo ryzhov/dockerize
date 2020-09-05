2c9370c35a780ec8bbd2d0048c76822d591c4c88
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2020-09-05 21:51:59 +0300

    update docker env

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 
 . ./.env
 
-DOCKER_FILE=.docker/dev/Dockerfile
+DOCKER_FILE=.docker/Dockerfile
 IMAGE=${REGISTRY}/php-${ENV}:${VERSION}
+PHP_INI=`[ $ENV = "dev" ] && echo "php.ini-development" || echo "php.ini-production"`
 
-docker image build -t ${IMAGE} -f ${DOCKER_FILE}  .
+echo "build php-fpm; env => ${ENV} php.ini => ${PHP_INI}"
+
+docker image build -t ${IMAGE} -f ${DOCKER_FILE} --build-arg PHP_INI=${PHP_INI} --build-arg ENV=${ENV} .
 
 [ $? != 0 ] && echo "build image fail, exit." &&  exit 1
 
 echo ${IMAGE}
-
