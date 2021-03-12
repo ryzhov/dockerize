58ce79b6039dde4174d457dbfb8905b302e7d4e6
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2021-03-12 19:47:59 +0300

    upd node and scripts

--- node/build.sh
+++ node/build.sh
@@ -6,11 +6,12 @@ IMAGE=${REGISTRY}/node-${ENV}:${VERSION}
 USER_NAME=`id -u -n`
 USER_ID=`id -u`
 
-echo "build node; env => ${ENV}; image => ${IMAGE}; user name => ${USER_NAME}; user id => ${USER_ID}"
+echo "env => ${ENV}; image => ${IMAGE}; user name => ${USER_NAME}; user id => ${USER_ID}"
 
 docker image build -t ${IMAGE} \
     --build-arg USER_NAME=${USER_NAME} \
     --build-arg USER_ID=${USER_ID} \
+    --build-arg VERSION=${VERSION} \
     .
 
 [ $? != 0 ] && echo "build image fail, exit." &&  exit 1
