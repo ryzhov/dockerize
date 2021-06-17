c153504c1bc10f49a3167fadace5a766c047dc07
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2021-06-17 15:09:24 +0300

    update node image build script

--- node/build.sh
+++ node/build.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 . ./.env
+. ./.env.local
 
 IMAGE=${REGISTRY}/node-${ENV}:${VERSION}
 USER_NAME=`id -u -n`
 USER_ID=`id -u`
 
-echo "env => ${ENV}; image => ${IMAGE}; user name => ${USER_NAME}; user id => ${USER_ID}"
+echo "image => ${IMAGE}; user name => ${USER_NAME}; user id => ${USER_ID}"
+set -xe
 
-docker image build -t ${IMAGE} \
-    --build-arg USER_NAME=${USER_NAME} \
-    --build-arg USER_ID=${USER_ID} \
-    --build-arg VERSION=${VERSION} \
+docker image build -t "${IMAGE}" \
+    --build-arg USER_NAME="${USER_NAME}" \
+    --build-arg USER_ID="${USER_ID}" \
+    --build-arg VERSION="${VERSION}" \
     .
-
-[ $? != 0 ] && echo "build image fail, exit." &&  exit 1
-
-echo ${IMAGE}
