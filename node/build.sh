8977ea4d94ae13d97811cec0bfe982f32021f4fd
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2021-12-14 13:11:22 +0300

    update node dockerfile

--- node/build.sh
+++ node/build.sh
@@ -7,7 +7,7 @@ IMAGE=${REGISTRY}/node-${ENV}:${VERSION}
 USER_NAME=`id -u -n`
 USER_ID=`id -u`
 
-echo "image => ${IMAGE}; user name => ${USER_NAME}; user id => ${USER_ID}"
+echo "image => ${IMAGE}; version => ${VERSION}; user name => ${USER_NAME}; user id => ${USER_ID}"
 set -xe
 
 docker image build -t "${IMAGE}" \
