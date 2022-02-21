3b7c7b62dc5421321679f54f16454d21f68e34f3
Aleksandr N. Ryzhov <a.n.ryzhov@gmail.com>
2022-02-21 15:34:02 +0300

    slim and dev images

--- node/build.sh
+++ node/build.sh
@@ -3,14 +3,17 @@
 . ./.env
 . ./.env.local
 
+DOCKERFILE=Dockerfile."${ENV}"
 IMAGE=${REGISTRY}/node-${ENV}:${VERSION}
 USER_NAME=$(id -u -n)
 USER_ID=$(id -u)
 
+[ -f "${DOCKERFILE}" ] || { echo "dockerfile not found => ${DOCKERFILE}"; exit 1; }
+
 echo "image => ${IMAGE}; version => ${VERSION}; user name => ${USER_NAME}; user id => ${USER_ID}"
 set -xe
 
-docker image build -t "${IMAGE}" \
+docker image build -t "${IMAGE}" -f Dockerfile."${ENV}" \
     --build-arg USER_NAME="${USER_NAME}" \
     --build-arg USER_ID="${USER_ID}" \
     --build-arg VERSION="${VERSION}" \
